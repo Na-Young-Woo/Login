589adc7d460143d75f2bd30a63b1b0c5879d4cd6
woonayoung <dn2255520@gmail.com>
2020-12-10 23:37:03 +0900

    first push

--- login/src/App.js
+++ login/src/App.js
@@ -1,5 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import LandingPage from "./components/views/LandingPage";
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
         >
           Learn React
         </a>
+        <LandingPage></LandingPage>
       </header>
     </div>
   );
---
+++ login/src/components/views/LandingPage.js
@@ -0,0 +1,17 @@
+import React from "react";
+import styled from "styled-components";
+
+const LoginBlock = styled.div`
+  height: 100px;
+  background: red;
+`;
+
+function LandingPage() {
+  return (
+    <div>
+      <LoginBlock>ddd</LoginBlock>
+    </div>
+  );
+}
+
+export default LandingPage;
