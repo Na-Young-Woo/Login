a1e0b37610a9da429a2715953afc00b73be5e59c
woonayoung <dn2255520@gmail.com>
2020-12-11 00:22:22 +0900

    redux 설정 중

--- login/src/index.js
+++ login/src/index.js
@@ -4,10 +4,29 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
+// import list
+import { Provider } from "react-redux";
+import { applyMiddleware, createStore } from "redux";
+
+import promiseMiddlerware from "redux-promise";
+import reduxThunk from "redux-thuck";
+
+const createStoreWidthMiddleware = applyMiddleware(
+  promiseMiddlerware,
+  reduxThunk
+)(createStore);
+
 ReactDOM.render(
   <React.StrictMode>
-    {/* <Provider> */}
-    <App />
+    {/* <Provider
+      store={createStoreWidthMiddleware()
+      // 리듀서 생성 후 여기에 넣어준다.
+
+      //개발자 도구를 사용하기 위한 설정
+      
+      }
+    > */}
+      <App />
     {/* </Provider> */}
   </React.StrictMode>,
 
